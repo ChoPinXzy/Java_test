2d8da2ee8b5ff6169ff37eae3fcf2aacb6bcff8c
chopinxing <y7064521432@163.com>
2021-11-24 15:36:58 +0800

    测试提交代码到自己的GitHub

---
+++ src/main/java/test/lianxi.java
@@ -0,0 +1,19 @@
+package test;
+
+public class lianxi {
+    /*public static void main(String[] args) {
+        int a = (int) 6000000000L;
+        System.out.println(a);
+
+        byte b = 12;
+        byte c = 10;
+        int ss = b+c;
+
+    }*/
+
+    public static void main(String[] args) {
+        char zifu1 = '1';
+        System.out.println(zifu1+0);
+    }
+
+}
